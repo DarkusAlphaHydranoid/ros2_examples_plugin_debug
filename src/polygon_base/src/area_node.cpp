91c9aff2302b3f80441782b31412b2b2746a02ad
DarkusAlphaHydranoid <jarbay910@gmail.com>
2024-12-07 20:58:18 +0300

    Update area_node.cpp

--- src/polygon_base/src/area_node.cpp
+++ src/polygon_base/src/area_node.cpp
@@ -1,29 +1,21 @@
 #include <pluginlib/class_loader.hpp>
 #include <polygon_base/regular_polygon.hpp>
 
+#include <iostream>
+
 int main(int argc, char** argv)
 {
   // To avoid unused parameter warnings
   (void) argc;
   (void) argv;
 
-  pluginlib::ClassLoader<polygon_base::RegularPolygon> poly_loader("polygon_base", "polygon_base::RegularPolygon");
+  std::shared_ptr<pluginlib::ClassLoader<polygon_base::RegularPolygon>> poly_loader 
+	  = std::make_shared<pluginlib::ClassLoader<polygon_base::RegularPolygon>>("polygon_base", "polygon_base::RegularPolygon");
 
-  try
-  {
-    std::shared_ptr<polygon_base::RegularPolygon> triangle = poly_loader.createSharedInstance("polygon_plugins::Triangle");
-    triangle->initialize(10.0);
+  std::unique_ptr<polygon_base::RegularPolygon> triangle 
+	  = std::unique_ptr<polygon_base::RegularPolygon>(poly_loader->createUnmanagedInstance("polygon_plugins::Triangle"));
 
-    std::shared_ptr<polygon_base::RegularPolygon> square = poly_loader.createSharedInstance("polygon_plugins::Square");
-    square->initialize(10.0);
-
-    printf("Triangle area: %.2f\n", triangle->area());
-    printf("Square area: %.2f\n", square->area());
-  }
-  catch(pluginlib::PluginlibException& ex)
-  {
-    printf("The plugin failed to load for some reason. Error: %s\n", ex.what());
-  }
+  poly_loader.reset();
 
   return 0;
 }
